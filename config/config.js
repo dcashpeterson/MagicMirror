4225e4fcd20eb9469bb827cf8fe516c338f94841
Derek <derekcp@sympraxisconsulting.com>
2019-12-30 10:07:27 -0500

    Added Calendar

--- config/config.js
+++ config/config.js
@@ -9,21 +9,21 @@
  */
 
 var config = {
-	address: "0.0.0.0", // Address to listen on, can be:
+	address: "localhost", // Address to listen on, can be:
 	// - "localhost", "127.0.0.1", "::1" to listen on loopback interface
 	// - another specific IPv4/6 to listen on a specific interface
 	// - "", "0.0.0.0", "::" to listen on any interface
 	// Default, when address config is left out, is "localhost"
 	port: 8080,
-	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1", "192.168.1.13", "192.168.1.3", "192.168.1.7"], // Set [] to allow all IP addresses
+	ipWhitelist: ["127.0.0.1", "::ffff:127.0.0.1", "::1"], // Set [] to allow all IP addresses
 	// or add a specific IPv4 of 192.168.1.5 :
 	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.1.5"],
 	// or IPv4 range of 192.168.3.0 --> 192.168.3.15 use CIDR format :
 	// ["127.0.0.1", "::ffff:127.0.0.1", "::1", "::ffff:192.168.3.0/28"],
 
 	language: "en",
-	timeFormat: 12,
-	units: "imperial",
+	timeFormat: 24,
+	units: "metric",
 
 	modules: [
 		{
@@ -35,69 +35,42 @@ var config = {
 		},
 		{
 			module: "clock",
-			position: "top_left",
-
-			config: {
-				displaySeconds: false,
-				showDate: true,
-				timezone: "America/New_York",
-				displayType: "digital",
-
-			}
-
+			position: "top_left"
 		},
 		{
-			module: "calendar",
-			header: "Dereks Calendar",
+			module: "responsivecal",
+			header: "US Holidays Responsive",
 			position: "top_left",
 			config: {
-				maximumNumberOfDays: 2,
-				fade: false,
-				showEnd: true,
-				maxTitleLength: 50,
-				timeFormat: "dateheaders",
-				getRelative: 48,
 				calendars: [
 					{
 						symbol: "calendar-check",
-						url: "https://calendar.google.com/calendar/ical/derek.cashpeterson%40gmail.com/private-2914188070a964c03fac37b52b27aaca/basic.ics"
-
+						url: "webcal://www.calendarlabs.com/ical-calendar/ics/76/US_Holidays.ics"
 					}
-
 				]
 			}
 		},
-
-
-
+		{
+			module: "compliments",
+			position: "lower_third"
+		},
 		{
 			module: "currentweather",
 			position: "top_right",
-			header: "Weather for Today",
 			config: {
-				location: "Boston, Massachusetts",
-				locationID: "4943629",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "ec9661e5df00c8abc319f91d8bf3cc6f",
-				degreeLabel: "true",
-				showHumidity: "true",
-				showFeelslike: "true"
-
+				location: "New York",
+				locationID: "",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "YOUR_OPENWEATHER_API_KEY"
 			}
 		},
 		{
 			module: "weatherforecast",
-			position: "bottom_right",
+			position: "top_right",
 			header: "Weather Forecast",
 			config: {
-				location: "Boston, Massachusetts",
-				locationID: "4943629",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
-				appid: "ec9661e5df00c8abc319f91d8bf3cc6f",
-				fade: false,
-				colored: "true",
-				roundTemp: true,
-				maxNumberOfDays: 3,
-				tableClass: "medium"
-
+				location: "New York",
+				locationID: "5128581",  //ID from http://bulk.openweathermap.org/sample/city.list.json.gz; unzip the gz file and find your city
+				appid: "YOUR_OPENWEATHER_API_KEY"
 			}
 		},
 	{
@@ -113,16 +86,21 @@ config: {
 
 		},
 		{
-			module: 'MMM-BackgroundSlideshow',
-			position: 'fullscreen_below',
+			module: "newsfeed",
+			position: "bottom_bar",
 			config: {
-				imagePaths: ['modules/MMM-BackgroundSlideshow/exampleImages/'],
-				transitionImages: true,
-				randomizeImageOrder: true
+				feeds: [
+					{
+						title: "New York Times",
+						url: "http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml"
+					}
+				],
+				showSourceTitle: true,
+				showPublishDate: true,
+				broadcastNewsFeeds: true,
+				broadcastNewsUpdates: true
 			}
-		}
-
-
+		},
 	]
 
 };
